50e38694579dddd6a716f697557c9d475957bfda
mas0110 <masoudmohammed13@gmail.com>
2024-03-20 12:39:49 +0100

    stampa dei dati sulla welcome

--- resources/views/pages/welcome.blade.php
+++ resources/views/pages/welcome.blade.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>trains</title>
 </head>
 <body>
-    <h1>ciao</h1>
+    <h1>Trains</h1>
+    {{dd($trains)}}
 </body>
 </html>
